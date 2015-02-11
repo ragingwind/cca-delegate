82310c96421203c50a48e6f48d8ca20aced8553c
ragingwind <ragingwind@gmail.com>
2015-02-11 16:24:50 +0900

    Add version and checkenv

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+
+var spawn = require('child_process').spawn;
+var q = require('q');
+var nodeExec = require('exec');
+var semver = require('semver');
+
+function exec (bin, opt, cb) {
+  if (typeof opt === 'function') {
+    cb = opt;
+    opt = {};
+  }
+
+  nodeExec(bin, opt, function(err, out, code) {
+    cb({
+      stderr: err, stdout: out, exitcode: code
+    });
+  });
 };
+
+var ccad = {
+  cca: function() {
+    var deferer = q.defer();
+    exec(['cca', '--v'], function(std) {
+      var v = semver.valid(std.stdout.trim());
+      if (v) {
+        std.err = new Error('cca is not installed');
+      }
+
+      deferer[!std.err ? 'resolve' : 'reject'](std);
+    });
+    return deferer.promise;
+  },
+	checkenv: function() {
+    var deferer = q.defer();
+    exec(['cca', 'checkenv'], function(std) {
+      var res = null;
+      var rx = /(Android|iOS)(.*?)*/gi;
+
+      while ((res = rx.exec(std.stderr)) !== null) {
+        if (res[0].indexOf('properly') === -1) {
+          std.err = new Error('Platforms are not properly');
+          break;
+        }
+      }
+
+      deferer[!std.err ? 'resolve' : 'reject'](std);
+    });
+
+    return deferer.promise;
+	},
+
+}
+
+module.exports = ccad;
--- test/test.js
+++ test/test.js
@@ -1,11 +1,24 @@
 /*global describe, it */
 'use strict';
+
 var assert = require('assert');
 var ccad = require('../');
 
-describe('ccad node module', function () {
-  it('must have at least one test', function () {
-    ccad();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+require("mocha-as-promised")();
+
+describe('ccad', function () {
+  it('cca should return its version', function (done) {
+    return ccad.cca().then(function(res) {
+      assert(true);
+    }).catch(function(res) {
+      assert(false, 'cca ' + res.err.toString());
+    });
   });
+  // it('checkenv should has no error', function (done) {
+  //   return ccad.checkenv().then(function(res) {
+  //     assert(true);
+  //   }).catch(function(res) {
+  //     assert(false, 'checkenv ' + res.err.toString());
+  //   });
+  // });
 });
