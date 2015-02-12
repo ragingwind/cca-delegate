dc15ff521b008ab586f223707099ef0afb12aa0c
ragingwind <ragingwind@gmail.com>
2015-02-12 19:49:57 +0900

    Add platform

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ var q = require('q');
 var nodeExec = require('exec');
 var semver = require('semver');
 
+var errorMessages = {
+  'checkenv': [
+    'properly'
+  ],
+  'create': [
+    'Path already exists',
+    'App id contains a reserved word'
+  ],
+  'platform': [
+    'is not a Cordova-based project'
+  ]
+};
+
+function hasError(cmd, stderr) {
+  if (!stderr) return false;
+
+  var errs = errorMessages[cmd];
+
+  for (var i = 0; i < errs.length; ++i) {
+    if (stderr.indexOf(errs[i]) !== -1) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 function exec (bin, opt, cb) {
   if (typeof opt === 'function') {
     cb = opt;
@@ -18,12 +45,20 @@ function exec (bin, opt, cb) {
   });
 };
 
+function isPackageName(name) {
+  return /^([a-zA-Z]{2,10})\.([a-zA-Z]{1,6}|[a-zA-Z0-9-]{1,30})\.[a-zA-Z0-9-_]{0,61}/gi.test(name);
+}
+
+function isSupportedPlatform(platform) {
+  return /^android$/i.test(platform) || /^ios$/i.test(platform);
+}
+
 var ccad = {
-  cca: function() {
+  version: function() {
     var deferer = q.defer();
     exec(['cca', '--v'], function(std) {
-      var v = semver.valid(std.stdout.trim());
-      if (v) {
+      std.version = semver.valid(std.stdout.trim());
+      if (!std.version) {
         std.err = new Error('cca is not installed');
       }
 
@@ -33,12 +68,13 @@ var ccad = {
   },
 	checkenv: function() {
     var deferer = q.defer();
+
     exec(['cca', 'checkenv'], function(std) {
       var res = null;
       var rx = /(Android|iOS)(.*?)*/gi;
 
       while ((res = rx.exec(std.stderr)) !== null) {
-        if (res[0].indexOf('properly') === -1) {
+        if (!/properly/i.test(res[0])) {
           std.err = new Error('Platforms are not properly');
           break;
         }
@@ -49,7 +85,69 @@ var ccad = {
 
     return deferer.promise;
 	},
+  create: function(opt) {
+    var deferer = q.defer();
 
+    if (!opt || !opt.directory || !opt.name) {
+      throw new Error('Missing arguments to create a project');
+    }
+
+    if (!isPackageName(opt.name)) {
+      throw new Error('Invalid package name');
+    }
+
+    if (opt.platform && isSupportedPlatform(opt.platform)) {
+      throw new Error('It is invalid platform ' + opt.platform)
+    }
+
+    var args = ['cca', 'create'];
+
+    args.push(opt.directory);
+    args.push(opt.name);
+    opt.platform && args.push('--' + opt.platform);
+    opt.copyFrom && args.push('--copy-from' + opt.copyFrom);
+    opt.linkTo && args.push('--link-to' + opt.linkTo);
+
+    exec(args, function(std) {
+      if (hasError('create', std.stderr, 0)) {
+        std.err = new Error('Project creating failed, Reason: ' + std.stderr);
+      }
+
+      deferer[!std.err ? 'resolve' : 'reject'](std);
+    });
+
+    return deferer.promise;
+  },
+  // @todo make args as array
+  addPlatform: function(platform) {
+    var deferer = q.defer();
+
+    if (!isSupportedPlatform(platform)) {
+      throw new Error('It is invalid platform ' + platform)
+    }
+
+    var args = ['cca', 'platform', 'add', platform];
+
+    exec(args, function(std) {
+      if (hasError('platform', std.stderr)) {
+        std.err = new Error('Platform add failed');
+      }
+
+      deferer[!std.err ? 'resolve' : 'reject'](std);
+    });
+
+    return deferer.promise;
+  },
+  removePlatforms: function() {},
+  getPlatforms: function() {},
+  updatePlatforms: function() {},
+  addPlugins: function(plugins) {},
+  removePlugins: function() {},
+  getPlugins: function() {},
+  searchPlugins: function() {},
+  build: function() {},
+  run: function() {},
+  push: function() {}
 }
 
 module.exports = ccad;
--- test/test.js
+++ test/test.js
@@ -2,23 +2,88 @@
 'use strict';
 
 var assert = require('assert');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+var path = require('path');
 var ccad = require('../');
 
 require("mocha-as-promised")();
 
 describe('ccad', function () {
+  var tmp = path.join(__dirname, 'tmp');
+  var cwd = process.cwd();
+
+  before(function() {
+    rimraf.sync(tmp);
+    mkdirp(tmp);
+  });
+
+  beforeEach(function() {
+    process.chdir(cwd);
+  })
+
   it('cca should return its version', function (done) {
-    return ccad.cca().then(function(res) {
+    return ccad.version().then(function(res) {
+      console.log('cca version is', res.version);
       assert(true);
     }).catch(function(res) {
-      assert(false, 'cca ' + res.err.toString());
+      assert(false, res.err.toString());
     });
   });
-  // it('checkenv should has no error', function (done) {
-  //   return ccad.checkenv().then(function(res) {
-  //     assert(true);
-  //   }).catch(function(res) {
-  //     assert(false, 'checkenv ' + res.err.toString());
-  //   });
-  // });
+
+  it('Environments should be prepared', function (done) {
+    return ccad.checkenv().then(function(res) {
+      assert(true);
+    }).catch(function(res) {
+      assert(false, res.err.toString());
+    });
+  });
+
+  it('New project should be created', function (done) {
+    return ccad.create({
+      directory: path.join(tmp, 'myApp'),
+      name: 'com.company.myapp'
+    }).then(function(res) {
+      assert(true);
+    }).catch(function(res) {
+      assert(false, res.err.toString());
+    });
+  });
+
+  it('New project should be failed', function (done) {
+    return ccad.create({
+      directory: path.join(tmp, 'myApp'),
+      name: 'com.company.myapp',
+    }).then(function(res) {
+      assert(false, res.err.toString());
+    }).catch(function(res) {
+      assert(true);
+    });
+  });
+
+  it('New project should be failed', function (done) {
+    return ccad.create({
+      directory: path.join(tmp, 'myApp1'),
+      name: 'com.do.1app'
+    }).then(function(res) {
+      assert(false, res.err.toString());
+    }).catch(function(res) {
+      assert(true);
+    });
+  });
+
+  it('Platform should be added', function (done) {
+    var target = path.join(tmp, 'myApp2');
+    return ccad.create({
+      directory: target,
+      name: 'com.company.myap2',
+    }).then(function() {
+      process.chdir(target);
+      return ccad.addPlatform('android');
+    }).then(function(res) {
+      assert(true);
+    }).catch(function(res) {
+      assert(false, res.err.toString());
+    });
+  });
 });
